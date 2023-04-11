2743d144e0ec03e9060751bbc1f321df39725ba3
hfkhoo02 <130447209+hfkhoo02@users.noreply.github.com>
2023-04-11 16:42:45 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.cyq6ppvuh1ze.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "khoohongfung-employee"
+customregion = "us-east-1"
